73d3fce3b646d8c5db7f6a5e60b4a3d1e0ee3905
MishMick <mishallovic@gmail.com>
2015-10-02 18:56:59 +0530

    02/10/2015

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+    void Start()
+    { 
+     if(autoLoadNextLevelAfter !=0)
+     Invoke ("LoadNextLevel" , autoLoadNextLevelAfter);
+    }
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+	public void LoadNextLevel()
+	{
+	 Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour 
+{
+	public AudioClip[] myAudioArray;
+	private AudioSource audioSource;
+	void Awake()
+	{
+	 DontDestroyOnLoad(gameObject);
+	}
+	void Start()
+	{
+	 audioSource = GetComponent<AudioSource>();
+	}
+	void OnLevelWasLoaded(int level)
+	{
+	 AudioClip thisLevelMusic = myAudioArray[level];
+	 if(thisLevelMusic)
+	 {
+	  audioSource.clip = thisLevelMusic;
+	  audioSource.Play();
+	  audioSource.loop = true;
+	 }
+	}	
+}
